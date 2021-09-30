bcc7ba613348ed157259905d9675a25d8bcfb8c6
Stalyn Chancay <stalyn151996@gmail.com>
2021-09-29 22:18:01 -0500

    updated source for PSO and QPSO

---
+++ src/pso/test_pso.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.datasets import load_iris
+from neuralNetwork.deepNeuralNetwork import neuralNetwork
+
+from sklearn.metrics import mean_squared_error
+
+
+# load the data
+data = load_iris()
+X = data.data
+y = data.target
+#y = np.array(y).reshape((len(y),1))
+
+nn = neuralNetwork()
+#loss1 = nn.train(X, y)
+#print(loss1)
+#cost_pred = 1.0 - (nn.train(X, y) == y).mean()
+y_pred = nn.train(X, y)
+cost_pred1 = mean_squared_error(y, y_pred)
+
+print('value prediction: ', cost_pred1)
+#print('shape of plotting: ', np.array(nn.metric_loss)[0].shape)
+
+#plotting
+x_plot = np.arange(1000)
+y_plot = []
+
+for i in range(1000):
+    y_plot.append(nn.metric_loss[i][0])
+
+fig1, ax1 = plt.subplots()
+ax1.plot(x_plot, y_plot)
+ax1.set_xlabel('epoch')
+ax1.set_ylabel('loss')
+ax1.set_title("The evolution of the loss during the trainning with PSO")
+plt.savefig("loss.png")
+plt.show()
