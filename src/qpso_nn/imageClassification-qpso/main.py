a9ed5407b03874e34666e6002193e75429fafc95
uliana <uliana@uliana.uliana.net>
2022-03-07 15:29:37 -0500

    img Classification code

---
+++ src/qpso_nn/imageClassification-qpso/main.py
@@ -0,0 +1,106 @@
+#---------------
+# import modules
+#---------------
+
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+from os import listdir
+from qpso.qpso import QDPSO
+from sklearn.metrics import mean_squared_error
+from matplotlib import image #loading img
+from skimage import color #gray-scale
+#from skimage.transform import resize, rescale
+
+from neuralNetwork.perceptron import perceptron 
+
+#----------------
+#Global variables
+#----------------
+
+root_path = './dataset/'
+train_path = 'seg_train/'
+#test_path = 'seg_test/seg_test/'
+#pred_path = 'seg_pred/seg_pred/'
+
+# Dataset classes: buildings, forest, glacier, mountain, sea, street
+data_clases = np.array(['buildings', 
+						'forest', 
+						'glacier', 
+						'mountain', 
+						'sea', 
+						'street'])
+IMG_HEIGHT = 30
+IMG_WIDTH = 30
+
+#---------------------------------
+# prepare training and testing set
+#---------------------------------
+
+X_train_aux = []
+y_train = []
+
+for c in range(len(data_clases)):
+	for filename in listdir(root_path + train_path + data_clases[c] + '/'):
+		# load image
+		img_data = image.imread(root_path + train_path + data_clases[c] + '/' + filename)
+		img_data = color.rgb2gray(img_data)
+		img_data = cv2.resize(img_data, (IMG_HEIGHT,IMG_WIDTH), interpolation=cv2.INTER_CUBIC)
+		#img_data = np.resize(img_data, (IMG_HEIGHT,IMG_WIDTH))
+		# store loaded image
+		X_train_aux.append(img_data)
+		y_train.append(c)
+		print('> loaded %s %s' % (filename, img_data.shape))
+
+# convert to array (float 64)
+X_train_aux = np.array(X_train_aux, dtype=float)
+y_train = np.array(y_train)
+
+# normalize data
+#X_train_aux = X_train_aux / 255
+
+print('X_train shape: ', X_train_aux.shape)
+print('y_train shape: ', y_train.shape)
+
+# reshape X_train for qpso algorithm
+
+"""X_train = np.reshape(X_train_aux[0], (1, -1))
+
+for i in range(1, len(X_train_aux)):
+    X_train_reshape = np.reshape(X_train_aux[i], (1, -1))
+    X_train = np.concatenate((X_train, X_train_reshape))
+    print('reshape img: ', i)"""
+
+X_train = X_train_aux.reshape(X_train_aux.shape[-3], np.prod(X_train_aux.shape[-2:]))
+print('Reshape X_train for QDPSO: ', X_train.shape)
+
+#print('check values of the input: ', X_train[1])
+#print('reshape X_train input: ', X_train.shape)
+
+# qdpso neural network algorithm
+
+#----------------
+# Load perceptron
+#----------------
+X_sample = len(X_train) 
+X_input = IMG_HEIGHT * IMG_WIDTH
+X_class = len(data_clases)
+
+nn = perceptron(X_sample, X_input, X_class)
+
+#---------------------------------------
+# variable initialize for qpso algorithm
+#---------------------------------------
+
+beta = 1.13 #1.7
+n_particulas = 25
+max_iter = 1000
+gBest = nn.train(X_train, y_train, beta, n_particulas, max_iter)
+print('best value', gBest)
+
+np.save('gBest_113_25_1000_1_30.npy', gBest)
+
+y_pred = np.argmax(nn.forward(X_train, gBest), axis=1)
+cost_pred1 = mean_squared_error(y_train, y_pred)
+print ('prediction cost with training',cost_pred1)
---
+++ src/qpso_nn/imageClassification-qpso/neuralNetwork/perceptron.py
@@ -0,0 +1,163 @@
+#--------------
+#import modules
+#--------------
+
+import numpy as np
+
+from qpso.qpso import QDPSO
+
+#-------------------------
+#Activation function class
+#-------------------------
+
+class activationFunction(object):
+    def __init__(self, X_sample):
+        self.n_sample_af = X_sample
+
+    def softmax(self, z):
+        """
+        Apply softmax function on logits and return probabilities.
+        :param logits: double(N, C)
+        Logits of each instance for each class.
+        :return: double(N, C)
+        probability for each class of each instance.
+        """
+        exps = np.exp(z)
+        return exps / np.sum(exps, axis=1, keepdims=True)
+    
+    def sigmoid(self, z):
+        #sig = np.where(z<0, np.exp(z)/(1 + np.exp(z)), 1/(1 + np.exp(-z)))
+        #return sig
+        """#Numerically stable sigmoid function.
+        if x >= 0:
+            z = exp(-x)
+            return 1 / (1 + z)
+        else:
+            # if x is less than zero then z will be small, denom can't be
+            # zero because it's 1+z.
+            z = exp(x)
+            return z / (1 + z)"""
+        return np.exp(-np.logaddexp(0, -z))
+
+#--------------------
+#Loss functions class
+#--------------------
+
+class lossFunction(object):
+    def __init__(self, X_sample):
+        self.n_sample_lf = X_sample
+
+    def NLL(self, y, y_hat):
+        #correct_logprobs = - np.log(z)
+        #return np.sum(correct_logprobs) / self.n_sample_lf
+        corect_logprobs = -np.log(y_hat[range(self.n_sample_lf), y])
+        return np.sum(corect_logprobs) / self.n_sample_lf
+    
+    def crossEntropy(self, y, y_hat):
+        """
+        Calculates Categorical Cross Entropy loss.
+        :param probs: double(N, C)
+            Probability of each instance for each class.
+        :param Y: int(N, C)
+            One-hot encoded representation of classes.
+        :return: double
+            Returns value of loss calculated.
+        """
+        #num_samples = len(y_hat)
+        ind_loss = np.max(-1 * y * np.log(y_hat + 1e-12), axis=1)
+        return np.sum(ind_loss) / self.n_sample_lf
+
+    def mse(self, y, y_hat):
+        return np.sum(0.5*(y - y_hat)**2)
+
+#-----------------
+# Perceptron Class
+#-----------------
+
+class perceptron(object):
+    def __init__(self, X_sample, X_input, X_class):
+        # initialize parameters
+        self.n_inputs = X_input
+        self.n_hidden = X_input * 3
+        self.n_outputs = X_class
+        self.n_sample = X_sample
+        self.af = activationFunction(X_sample)
+        self.lf = lossFunction(X_sample)
+        self.metric_loss = []
+        self.d = (self.n_inputs * self.n_hidden) + (self.n_hidden * self.n_outputs) + self.n_hidden + self.n_outputs
+
+        # Weight and Bias parameters
+        self.W1 = np.random.randn(self.n_inputs, self.n_hidden)
+        self.W2 = np.random.randn(self.n_hidden, self.n_outputs)
+        self.b1 = np.random.randn(self.n_hidden, )
+        self.b2 = np.random.randn(self.n_outputs, )
+    
+    def forward(self, X, *p):
+        if (len(p)==0):
+            W1 = self.W1
+            b1 = self.b1
+            W2 = self.W2
+            b2 = self.b2
+        else:
+            p = np.array(p).reshape((self.d, 1))
+            W1_lim = self.n_inputs * self.n_hidden
+            W1 = p[0:W1_lim].reshape((self.n_inputs, self.n_hidden))
+            b1_lim = W1_lim + self.n_hidden
+            b1 = p[W1_lim:b1_lim].reshape((self.n_hidden, ))
+            W2_lim = b1_lim + (self.n_hidden * self.n_outputs)
+            W2 = p[b1_lim:W2_lim].reshape((self.n_hidden, self.n_outputs))
+            b2_lim = W2_lim + self.n_outputs
+            b2 = p[W2_lim:b2_lim].reshape((self.n_outputs, ))
+
+        z1 = np.dot(X, W1) + b1
+        a1 = np.tanh(z1)
+        z2 = np.dot(a1, W2) + b2
+        y_hat = self.af.softmax(z2)
+        return y_hat
+    
+    def backware(self, p):
+        y_hat = self.forward(self.X, p)
+        loss = self.lf.crossEntropy(self.y, y_hat)
+        #self.metric_loss.append(loss)
+        return loss
+
+    def f(self, x):
+        j = self.backware(x)
+        return j
+    
+    def log(self, s):
+        best_value = [p.best_value for p in s.particles()]
+        best_value_avg = np.mean(best_value)
+        best_value_std = np.std(best_value)
+        self.metric_loss.append(s.gbest_value)
+        print("{0: >5}  {1: >9.3E}  {2: >9.3E}  {3: >9.3E}".format(s.iters, s.gbest_value, best_value_avg, best_value_std))
+
+    def one_hot_encode(self, Y):
+        """
+        create one-hot encoded vectors from target labels(Y).
+        :param Y: int(N, )
+        :return: int(N, C)
+            Returns an array of shape(N, C) where C is number of classes.
+        """
+        num_unique = len(np.unique(np.array(Y)))
+        zeros = np.zeros((len(Y), num_unique))
+        zeros[range(len(Y)), Y] = 1
+        return zeros
+
+    def train(self, X, y, beta, n_particulas, max_iter):
+        print("training nn with QDPSO")
+        self.X = X
+        self.y = self.one_hot_encode(y)
+
+        #initalize parameters for qdpso 
+        NParticle = n_particulas
+        MaxIters = max_iter
+        bounds = [(-1, 1) for i in range(0, self.d)]
+        g = beta #1.7 #.96
+
+        s = QDPSO(self.f, NParticle, self.d, bounds, MaxIters, g)
+        s.update(callback=self.log, interval=1)
+
+        np.save('bValue_113_25_100_1_30.npy', np.array(self.metric_loss))
+        
+        return s.gbest
